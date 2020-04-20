bac2f359ec1e059b5f04553e85b6e451fa2335c0
hatiolab <shortstop@hatiolab.com>
2020-04-20 21:46:01 +0900

    Fix

--- src/main/java/xyz/anythings/gw/entity/Deployment.java
+++ src/main/java/xyz/anythings/gw/entity/Deployment.java
@@ -14,7 +14,7 @@ import xyz.elidom.util.BeanUtil;
 import xyz.elidom.util.ValueUtil;
 
 @Table(name = "deployments", idStrategy = GenerationRule.UUID, indexes = {
-	@Index(name = "ix_deployments_0", columnList = "target_type,target_id,version,domainId")
+	@Index(name = "ix_deployments_0", columnList = "target_type,target_id,version,domain_id")
 })
 public class Deployment extends xyz.elidom.orm.entity.basic.ElidomStampHook {
 	/**
